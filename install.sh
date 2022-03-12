21e25503682ce5a59d3175245826df9a2bab874d
Lewis Cowles <lewis+github@cowles.uk>
2022-03-12 04:01:08 +0000

    Suggested edits
    Aims to improve readability and reliability. As the tunnel.sh is expected to be in /root, this also gives a perhaps more user friendly error message if permissions are not enough.
    
    Comments so that lay-people can understand.

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Check required permissions
+if [ "$EUID" -ne 0 ]
+	then echo "Must be root"
+	exit
+fi
+
+# Take note of important execution state
 wd=`pwd`
+
+# File-System changes
 ln -s ${wd}/tunnel.service /etc/systemd/system/tunnel.service
+ln -s ${wd}/tunnel.sh /root/tunnel.sh
+
+# SystemD
 systemctl enable tunnel
 systemctl start tunnel
 systemctl status tunnel
